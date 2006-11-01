7359eb8ab148b6c9cbc8455e2d341f74040cc05d
Greg MacLellan <greg@mtechsolutions.ca>
2006-11-01 03:03:57 +0000

    Updated to 1.3.0 (Thanks Kerry), #1238

--- page.printextensions.php
+++ page.printextensions.php
@@ -16,7 +16,6 @@ $dispnum = 'printextensions'; //used for switch on config.php
 //isset($_REQUEST['action'])?$action = $_REQUEST['action']:$action='';
 
 ?>
-</div>
 
 <?php 
 $gresults = printextensions_allusers();
@@ -35,7 +34,7 @@ if (isset($_REQUEST['quietmode'])) {
     <script type="text/javascript"> 
 		<!--
 		// Disable browser's Back button on another pg being able to go back to this pg.
-		history.forward();
+		// history.forward();
 		//-->
     </script> 
 <!--[if IE]>
@@ -58,8 +57,10 @@ if (isset($_REQUEST['quietmode'])) {
 <?php 
 
 if (!$extdisplay) {
-	echo '<br><h2>'._("Company Directory").'</h2><table border=0 width=400>';
-        echo "<tr width=250><td><b>Name</b></td><td width=50 align=right><b>Extension</b></td><td width=100 align=right><b>Assigned 
+	echo '<br><h2>'._("Company Directory").'</h2><table border=0 
+width=500>';
+        echo "<tr width=250><td align=left><b>Name</b></td><td width=50 
+align=center><b>Extension</b></td><td width=200 align=center><b>Assigned 
 DID</b></td></tr>";
 echo "<tr><td colspan=3><hr noshade></td></tr>";
 	
@@ -67,10 +68,15 @@ echo "<tr><td colspan=3><hr noshade></td></tr>";
 
 if (isset($gresults)) {
         foreach ($gresults as $gresult) {
-		echo "<tr width=250><td>$gresult[1]</td><td width=50 align=right>$gresult[0]</td><td width=100 
+                $defined = is_array($set_users) ? (in_array($gresult[0], $set_users) ? "(edit)" : "(add)") : "add";
+		echo "<tr width=250><td>$gresult[1]</td><td width=50 
+align=right>$gresult[0]</td><td width=200 
 align=right>$gresult[2]</td></tr>";
         }
 }
 ?>
 </table>
+<p>
+<a href="http://aussievoip.com.au/wiki/freePBX-PrintExtensions"><font 
+size=1>Print Extensions v1.3</font></a>
 </div>
