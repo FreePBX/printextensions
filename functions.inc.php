952f49fa0adcf7ef2eeddf9a3a5d115242ac6dad
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2008-10-02 22:37:14 +0000

    remove content of functions.inc.php no longer used

--- functions.inc.php
+++ functions.inc.php
@@ -1,19 +1,4 @@
 <?php 
 /* $Id:$ */
 
-function printextensions_allusers() {
-        global $db;
-        $sql = "SELECT extension,name,directdid FROM users ORDER BY extension";
-        $results = $db->getAll($sql);
-        if(DB::IsError($results)) {
-                $results = null;
-        }
-        foreach($results as $result){
-                if (checkRange($result[0])){
-                        $users[] = array($result[0],$result[1],$result[2]);
-                }
-        }
-        if (isset($users)) sort($users);
-        return $users;
-}
 ?>
